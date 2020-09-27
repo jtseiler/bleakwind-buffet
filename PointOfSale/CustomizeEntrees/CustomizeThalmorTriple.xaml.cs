5bccd496773d31be6f0240bbaf5d416c82df39c1
Jack Seiler <jtseiler@ksu.edu>
2020-09-27 15:08:09 -0500

    customize files created

---
+++ PointOfSale/CustomizeEntrees/CustomizeThalmorTriple.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.CustomizeEntrees
+{
+    /// <summary>
+    /// Interaction logic for CustomizeThalmorTriple.xaml
+    /// </summary>
+    public partial class CustomizeThalmorTriple : UserControl
+    {
+        public CustomizeThalmorTriple()
+        {
+            InitializeComponent();
+        }
+    }
+}
